d0f1534f25cf59c3dc874d1874f62adb6a853e87
Orn <pq781137@corp.westworlds.com>
2020-12-29 09:06:46 -0600

    Switched to using monkeypatch instead of additional mock library

--- tests/unit/test_rest_api.py
+++ tests/unit/test_rest_api.py
@@ -1,10 +1,13 @@
 import json
 import os
 import pytest
-import mock
+
+"""
+Using the mock library. It has the benifit of starting and stoping the mock os.environ
+#import mock
 
 @pytest.mark.event_json_file_name("./events/event_test_send_number_email.json")
-def test_send_number_email_email_contents(apigw_event_fixt, lambda_context):
+def test_send_number_email_email_contents(mocker, apigw_event_fixt, lambda_context):
     mock_env = mock.patch.dict(
         os.environ, {"FROM_EMAIL": "test@west.com", "SENDGRID_KEY": "local", "EXEC_ENV": "local"}
     )
@@ -12,6 +15,28 @@ def test_send_number_email_email_contents(apigw_event_fixt, lambda_context):
     from rest_api import app as flask_app
     ret = flask_app.app(apigw_event_fixt, lambda_context)
     mock_env.stop()
+
+    body = json.loads(ret["body"])
+    email = json.loads(body["email"].replace("'", '"'))
+    assert email["from"] == {"email": "test@west.com"}
+    assert email["subject"] == "Flask SendGrid Demo: Random Number Generated"
+    assert email["personalizations"] == [{"to": [{"email": "donovan.orn@west.com"}]}]
+    assert email["content"] == [
+        {
+            "type": "text/plain",
+            "value": "Hello donovan.orn@west.com your generated number was 12",
+        }
+    ]
+"""
+
+@pytest.mark.event_json_file_name("./events/event_test_send_number_email.json")
+def test_send_number_email_email_contents(mocker, apigw_event_fixt, lambda_context):
+    monkeypatch.setenv("FROM_EMAIL", "test@west.com")
+    monkeypatch.setenv("SENDGRID_KEY", "local")
+    monkeypatch.setenv("EXEC_ENV", "local")
+
+    from rest_api import app as flask_app
+    ret = flask_app.app(apigw_event_fixt, lambda_context)
 
     body = json.loads(ret["body"])
     email = json.loads(body["email"].replace("'", '"'))
