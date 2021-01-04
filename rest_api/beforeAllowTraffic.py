f027f7fbe24d930f8821b2cd489b35c55b0dfc96
Justin Clark <jpclark6@gmail.com>
2021-01-04 08:54:35 -0700

    refactor pipeline

--- rest_api/beforeAllowTraffic.py
+++ rest_api/beforeAllowTraffic.py
@@ -4,9 +4,8 @@ import boto3
 client = boto3.client('codedeploy')
 
 def lambda_handler(event, context):
-    print('Testing beforeAllowTraffic.py')
-    print('EVENT', event)
-    print('CONTEXT', context)
+    print('Testing BeforeAllowTraffic.py')
+
     deployment_id = event.get('DeploymentId')
     lifecycle_event_hook_execution_id = event.get('LifecycleEventHookExecutionId')
     response = client.put_lifecycle_event_hook_execution_status(
